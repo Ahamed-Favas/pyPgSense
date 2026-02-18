29351be1879347a989952bf1d56d08a0e9022bff
ahamedfavas <ahamedfavas@nucore.in>
2026-02-18 16:26:07 +0530

    added syntax higligting

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,11 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+    // This logs once when a Python file is opened (or whatever triggers your activation)
+    console.log('SQLens: SQL injection grammar for Python is now active.');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "sqlens" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('sqlens.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Sqlens!');
-	});
-
-	context.subscriptions.push(disposable);
+    // Usually, you don't need code here for TextMate grammars.
+    // In the future, you could add SQL formatting or "Execute SQL" commands here!
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
